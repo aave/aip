b26dc17abc7a10dfc34e8918e9e21af9135f4020
sendra <polsendra@gmail.com>
2021-08-10 13:23:17 +0200

    added schema validation support

---
+++ api-schema-validation.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const Ajv = require("ajv");
+const validationSchema = require('./aip-schema.json');
+
+const ajv = new Ajv() 
+const validate = ajv.compile(validationSchema)
+
+jsonAips = JSON.parse(fs.readFileSync('./content/ipfs-aips/all-aips.json').toString());
+Object.keys(jsonAips).forEach((id) => {
+  const idNumber = id.split('-')[1];
+  if (Number(idNumber) > 27) {
+    delete Object.assign(jsonAips[id], {'description': jsonAips[id]['content'] })['content'];
+    const valid = validate(jsonAips[id])
+
+    console.log(`AIP: ${id} schema is valid? ${valid}`);
+
+    if (!valid) {
+      console.log('Proposal: ', jsonAips[id]);
+      throw new Error(JSON.stringify(validate.errors))
+    }
+  }
+}); 
